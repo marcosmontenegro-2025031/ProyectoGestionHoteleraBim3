5dfc77745c205420fa4efe868d5ad07643e86b93
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-07-01 11:02:07 -0600

    fix: Se hicieron unos cambios en la clase detalle de reserva

--- ProyectoInnora/src/models/detalleReserva.ts
+++ ProyectoInnora/src/models/detalleReserva.ts
@@ -1,5 +1,5 @@
 export interface DetalleReserva {
     id_reserva: number,
-    reserva_id_reserva: number,
-    habitacion_id_habitacion: number
+    reserva_id_reserva: number | null,
+    habitacion_id_habitacion: number | null
 }
