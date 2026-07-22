43417295cfbf272393b6754b22f50d4cb84ad6be
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-07-22 11:49:49 -0600

    feat: Se crearon los archivos data de detalleReserva

---
+++ ProyectoInnora/src/data/detalleReservas.ts
@@ -0,0 +1,24 @@
+import { DetalleReserva } from "../models/detalleReserva";
+
+export const listaDetallesReserva: DetalleReserva[] = [
+    {
+        id_reserva: 1,
+        reserva_id_reserva: 101,
+        habitacion_id_habitacion: 201
+    },
+    {
+        id_reserva: 2,
+        reserva_id_reserva: 102,
+        habitacion_id_habitacion: null
+    },
+    {
+        id_reserva: 3,
+        reserva_id_reserva: null,
+        habitacion_id_habitacion: 203
+    },
+    {
+        id_reserva: 4,
+        reserva_id_reserva: 104,
+        habitacion_id_habitacion: 204
+    }
+];
